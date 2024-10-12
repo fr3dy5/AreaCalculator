99571d895327737a983b12cf7160f24461035cc3
fr3dy5 <fr3dy5al@gmail.com>
2024-10-11 20:22:32 -0500

    add opening line, first guess at input variable and other variables, switch cases structure inside do while loop

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("What type of shape (R)ectangle, (T)riangle, or (C)ircle?");
+
+string shape = Console.ReadLine()[0];
+
+double length;
+double width;
+double tribase;
+double height;
+double radius;
+double area;
+
+do {
+    switch (shape){
+        case 'R' 
+            break;
+        case 'T' 
+            break;
+        case 'C' 
+            break;
+        case 'E' 
+            break;
+        default 
+            Console.WriteLine("Invalid shape type!")
+            break;
+    }
+} while (shape != 'E');
